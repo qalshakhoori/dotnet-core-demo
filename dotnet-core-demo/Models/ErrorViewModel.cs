dc3e6529cf2e16fd0f57cdded7613d19a19c2e2c
qalshakhoori <qalshakhoori@moic.gov.bh>
2023-03-19 14:38:12 +0300

    Add project files.

---
+++ dotnet-core-demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnet_core_demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
